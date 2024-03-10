b8a14059856ed7550db10b4bede4d13abf089830
Syuichi Murashima <mura@vwnet.jp>
2024-03-10 11:42:48 +0900

    Linux & PS5 互換性確保

--- uninstall.ps1
+++ uninstall.ps1
@@ -2,7 +2,10 @@
 $ModuleName = "TSV2MarkdownTable"
 
 # Module Path
+if(($PSVersionTable.Platform -eq "Win32NT") -or ($PSVersionTable.Platform -eq $null)){
 $ModulePath = Join-Path (Split-Path $PROFILE -Parent) "Modules"
+}else{
+$ModulePath = Join-Path ($env:HOME) "/.local/share/powershell/Modules"}
 $RemovePath = Join-Path $ModulePath $ModuleName
 
 # Remove Direcory
